90f80f94fafa8213ee6e23f22401c647fedf3c94
Klemen Flegar <flegar.klemen@gmail.com>
2025-10-24 11:29:29 +0200

    /

--- 3.vaja/routes/igralci.js
+++ 3.vaja/routes/igralci.js
@@ -47,6 +47,73 @@ router.post("/", async (req, res, next) => {
   if (!vzdevek || !geslo || !ime || !priimek || !email) {
     return res.status(400).json({ message: "Manjkajo podatki za registracij" });
   }
+
+  try {
+    if (await utils.igralecObstaja(vzdevek)) {
+      return res
+        .status(400)
+        .json({ message: "Igralec z istim imenom ze obstaja" });
+    }
+    const hash_geslo = await bcrypt.hash(geslo, 10);
+
+    const sql =
+      "INSERT INTO igralec (vzdevek, geslo, ime, priimek, email) VALUES (?, ?, ?, ?, ?)";
+    const [results] = await pool.execute(sql, [
+      vzdevek,
+      hash_geslo,
+      ime,
+      priimek,
+      email,
+    ]);
+
+    if (results.affectedRows === 1) {
+      const urlVira = utils.urlVira(req, `/igralci/${vzdevek}`);
+      res.location(urlVira);
+      res.status(201).json({ message: "igralec uspesno dodan", url: urlVira });
+    } else {
+      res.status(500).json({ message: "dodajanje igralca ni bilo uspesno" });
+    }
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.put("/:vzdevek", async (req, res, next) => {
+  const vzdevek = req.params.vzdevek;
+  const { geslo, ime, priimek, email } = req.body; // pridobimo podatke iz telesa zahteve
+
+  try {
+    if (!(await utils.igralecObstaja(vzdevek))) {
+      res.status(404).json({ message: "Igralec ne obstaja" });
+    }
+
+    // Preverimo, če so prisotni vsi potrebni parametri
+    if (!geslo || !ime || !priimek || !email) {
+      return res.status(400).json({
+        message:
+          "Manjkajo podatki za posodabljanje uporabnika ali pa niso pravilno strukturirani!",
+      });
+    }
+
+    const hash_geslo = await bcrypt.hash(geslo, 10);
+    const sql =
+      "UPDATE igralec SET geslo=?, ime=?, priimek=?, email=? WHERE vzdevek=?";
+    const [result] = await pool.execute(sql, [
+      hash_geslo,
+      ime,
+      priimek,
+      email,
+      vzdevek,
+    ]);
+
+    if (result.affectedRows === 1) {
+      res.status(204).send();
+    } else {
+      res.status(500).json({ message: "Dodajanje igralca ni bilo uspešno!" });
+    }
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
